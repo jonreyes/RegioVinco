5011dd28a9b4c56710e64a008c5ee6bb18fed67e
Jon <jon.reyes@stonybrook.edu>
2016-07-02 02:56:34 -0400

    Modified Style Classes

--- RVMESimpleAppFramework/src/saf/components/AppStyleArbiter.java
+++ RVMESimpleAppFramework/src/saf/components/AppStyleArbiter.java
@@ -11,38 +11,19 @@ package saf.components;
 public interface AppStyleArbiter {
     // THESE ARE COMMON STYLE CLASSES WE'LL USE
 
-    /**
-     *
-     */
-        public static final String CLASS_BORDERED_PANE = "bordered_pane";
+    public static final String CLASS_BORDERED_PANE = "bordered_pane";
 
-    /**
-     *
-     */
+    public static final String CLASS_GRID_PANE = "grid_pane";
+
     public static final String CLASS_HEADING_LABEL = "heading_label";
 
-    /**
-     *
-     */
     public static final String CLASS_SUBHEADING_LABEL = "subheading_label";
 
-    /**
-     *
-     */
     public static final String CLASS_PROMPT_LABEL = "prompt_label";
 
-    /**
-     *
-     */
     public static final String CLASS_PROMPT_TEXT_FIELD = "prompt_text_field";
 
-    /**
-     *
-     */
     public static final String CLASS_FILE_BUTTON = "file_button";
     
-    /**
-     *
-     */
     public void initStyle();
 }
