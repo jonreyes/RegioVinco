e12d510e6c84285493d7186938aafcbc73c2ea10
Jon <jon.reyes@stonybrook.edu>
2016-07-10 11:59:08 -0400

    Added Test Path Constants

---
+++ RegioVincoMapEditor/src/rvme/RVMEConstants.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rvme;
+
+import static saf.settings.AppStartupConstants.PATH_WORK;
+
+/**
+ *
+ * @author xion
+ */
+public class RVMEConstants {
+    public static final String PATH_EXPORT = PATH_WORK+"export/";
+    public static final String PATH_RAW_MAP_DATA = PATH_WORK+"raw_map_data/";
+    public static final String PATH_WORLD = PATH_EXPORT+"The World/";
+    public static final String PATH_EUROPE = PATH_WORLD+"Europe/";
+    public static final String PATH_ANDORRA = PATH_EUROPE+"Andorra/";
+}
